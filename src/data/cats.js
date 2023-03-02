5f1814a659ecfdc8b5a9ac420b5ac58f3964cceb
Manav Misra <manavm@visionwebsoft.com>
2023-03-02 10:24:16 -0600

    Add 🐱 data

---
+++ src/data/cats.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: "Fluffykins",
+    description: "A cat that acts like a dog",
+    id: 1,
+  },
+  {
+    name: "Garfield",
+    description: "A cat that hates Mondays",
+    id: 2,
+  },
+  {
+    name: "Whiskers",
+    description: "A cat that is always hungry",
+    id: 3,
+  },
+  {
+    name: "Mr. Bigglesworth",
+    description: "A cat that is a demon",
+    id: 4,
+  },
+];
