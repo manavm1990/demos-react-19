988a720640ff10a3edd83e1cb48110d5e5ab126b
Manav Misra <manavm@visionwebsoft.com>
2023-03-07 06:25:55 -0600

    Add PropTypes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
+import PropTypes from "prop-types";
+
 export default function Header(
   // Props are passed as an object. We can destructure it to get the text prop.
   { text }
 ) {
-  return <h1 className="text-3xl font-bold underline">{text}</h1>;
+  return <h1 className="my-6 text-center text-3xl font-black">{text}</h1>;
 }
+
+Header.propTypes = {
+  text: PropTypes.string.isRequired,
+};
