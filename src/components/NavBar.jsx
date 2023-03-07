9ad323b8cfabdfebc419eabccbfe5276c32df47f
Manav Misra <manavm@visionwebsoft.com>
2023-03-06 20:29:18 -0600

    Add hard-coded navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+export default function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/cats">Cats</a>
+        </li>
+        <li>
+          <a href="/users">Users</a>
+        </li>
+        <li>
+          <a href="/groceries">Groceries</a>
+        </li>
+        <li>
+          <a href="/counter">Counter</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
